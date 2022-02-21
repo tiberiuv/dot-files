e53fc606e74fa3505e522051743c9adf03188acc
Tiberiu Voicu <ts.voicu@icloud.com>
2022-02-21 20:00:03 +0000

    reduce duplicate in packer

--- lua/plugins/install_plugins.lua
+++ lua/plugins/install_plugins.lua
@@ -1,32 +1,55 @@
-local function fn()
-    local use = use
-    use({ "wbthomason/packer.nvim" })
+local packages = {
+    ["wbthomason/packer.nvim"] = {},
+    ["tpope/vim-surround"] = {},
+    ["tpope/vim-sleuth"] = {},
+    ["tpope/vim-repeat"] = {},
+    ["tami5/lspsaga.nvim"] = {},
+    ["kosayoda/nvim-lightbulb"] = {},
+    ["gbrlsnchs/telescope-lsp-handlers.nvim"] = {},
+    ["npxbr/gruvbox.nvim"] = { requires = { "rktjmp/lush.nvim" } },
+    ["chr4/nginx.vim"] = { ft = ".conf" },
+    ["wavded/vim-stylus"] = { ft = { ".styl", "stylus" } },
+    ["tomlion/vim-solidity"] = { ft = ".sol" },
+    ["Vimjas/vim-python-pep8-indent"] = { ft = ".py" },
 
-    use({ "npxbr/gruvbox.nvim", requires = { "rktjmp/lush.nvim" } })
+    ["hashivim/vim-terraform"] = { ft = { ".hcl", ".tf", ".tfvars", ".terraformrc" } },
 
-    -- Languages and syntax highlighting
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
-        config = "require('plugins.treesitter')",
-    })
+    ["neovim/nvim-lspconfig"] = { config = "require('lsp')()" },
 
-    use({ "chr4/nginx.vim", ft = ".conf" })
-    use({ "wavded/vim-stylus", ft = { ".styl", "stylus" } })
-    use({ "tomlion/vim-solidity", ft = ".sol" })
-    use({ "Vimjas/vim-python-pep8-indent", ft = ".py" })
-    use({
-        "hashivim/vim-terraform",
-        ft = { ".hcl", ".tf", ".tfvars", ".terraformrc" },
-    })
+    ["mfussenegger/nvim-dap"] = { keys = "<leader>d", config = "require('plugins.dap')" },
 
-    use({
-        "neovim/nvim-lspconfig",
-        config = "require('lsp')()",
-    })
+    ["b3nj5m1n/kommentary"] = { keys = { "gc", "gcc" } },
 
-    use({
-        "hrsh7th/nvim-cmp",
+    ["ahmedkhalf/project.nvim"] = { config = "require('plugins.project')" },
+
+    ["iamcco/markdown-preview.nvim"] = { run = "cd app && yarn install" },
+
+    ["nvim-lua/plenary.nvim"] = { module = "plenary" },
+
+    ["nvim-telescope/telescope-fzf-native.nvim"] = { run = "make" },
+    ["nvim-telescope/telescope.nvim"] = { config = "require('plugins.telescope')" },
+
+    ["lewis6991/gitsigns.nvim"] = { config = "require('plugins.gitsigns')", event = "BufRead" },
+
+    ["janko-m/vim-test"] = { cmd = { "TestNearest", "TestFile", "TestSuite", "TestLatest" } },
+
+    ["antoinemadec/FixCursorHold.nvim"] = { event = "CursorHold" },
+
+    ["nvim-lua/lsp_extensions.nvim"] = { event = "BufEnter" },
+
+    ["akinsho/nvim-toggleterm.lua"] = { keys = "<C-t>", config = "require('plugins.toggle_term')" },
+
+    ["kabouzeid/nvim-lspinstall"] = { cmd = { "LspInstall", "LspUninstall" }, event = "BufEnter" },
+
+    ["kyazdani42/nvim-web-devicons"] = { module = "nvim-web-devicons" },
+
+    ["hoob3rt/lualine.nvim"] = { config = "require('plugins.lualine')", event = "VimEnter" },
+
+    ["jose-elias-alvarez/null-ls.nvim"] = { config = "require('plugins.null-ls')" },
+
+    ["folke/zen-mode.nvim"] = { config = "require('zen-mode').setup({})" },
+
+    ["hrsh7th/nvim-cmp"] = {
         requires = {
             { "L3MON4D3/LuaSnip" },
             { "hrsh7th/cmp-nvim-lsp" },
@@ -35,97 +58,39 @@ local function fn()
         },
         config = "require('plugins.cmp')",
         event = "InsertEnter",
-    })
+    },
 
-    use({
-        "mfussenegger/nvim-dap",
-        keys = "<leader>d",
-        config = "require('plugins.dap')",
-    })
+    ["nvim-treesitter/nvim-treesitter"] = {
+        run = ":TSUpdate",
+        config = "require('plugins.treesitter')",
+    },
 
-    use({ "tpope/vim-surround" })
-    use({ "tpope/vim-sleuth" })
-    use({ "tpope/vim-repeat" })
-
-    use({ "b3nj5m1n/kommentary", keys = { "gc", "gcc" } })
-
-    use({ "ahmedkhalf/project.nvim", config = "require('plugins.project')" })
-
-    use({
-        "iamcco/markdown-preview.nvim",
-        run = "cd app && yarn install",
-    })
-
-    use({ "nvim-lua/plenary.nvim", module = "plenary" })
-
-    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-    use({ "gbrlsnchs/telescope-lsp-handlers.nvim" })
-
-    use({
-        "nvim-telescope/telescope.nvim",
-        config = "require('plugins.telescope')",
-    })
-
-    use({
-        "lewis6991/gitsigns.nvim",
-        config = "require('plugins.gitsigns')",
-        event = "BufRead",
-    })
-
-    use({
-        "janko-m/vim-test",
-        cmd = { "TestNearest", "TestFile", "TestSuite", "TestLatest" },
-    })
-
-    use({ "antoinemadec/FixCursorHold.nvim", event = "CursorHold" })
-
-    use({ "nvim-lua/lsp_extensions.nvim", event = "BufEnter" })
-
-    use({
-        "akinsho/nvim-toggleterm.lua",
-        keys = "<C-t>",
-        config = "require('plugins.toggle_term')",
-    })
-
-    use({
-        "kabouzeid/nvim-lspinstall",
-        cmd = { "LspInstall", "LspUninstall" },
-        event = "BufEnter",
-    })
-
-    use({ "kyazdani42/nvim-web-devicons", module = "nvim-web-devicons" })
-
-    use({
-        "hoob3rt/lualine.nvim",
-        config = "require('plugins.lualine')",
-        event = "VimEnter",
-    })
-
-    use({
-        "kyazdani42/nvim-tree.lua",
+    ["kyazdani42/nvim-tree.lua"] = {
         keys = { "<C-n>" },
         config = "require('plugins.nvim-tree')",
         cmd = { "NvimTreeOpen", "NvimTreeToggle" },
-    })
+    },
 
-    use({
-        "karb94/neoscroll.nvim",
+    ["karb94/neoscroll.nvim"] = {
         keys = { "<C-u>", "<C-d>" },
         config = "require('plugins.neoscroll')",
-    })
+    },
+}
 
-    use({
-        "jose-elias-alvarez/null-ls.nvim",
-        config = "require('plugins.null-ls')",
-    })
-
-    use({ "tami5/lspsaga.nvim" })
-
-    use({
-        "folke/zen-mode.nvim",
-        config = "require('zen-mode').setup({})",
-    })
-    use({ "kosayoda/nvim-lightbulb" })
+local function fn()
+    for k, v in pairs(packages) do
+        use({
+            k,
+            requires = v.requires,
+            run = v.run,
+            config = v.config,
+            ft = v.ft,
+            event = v.event,
+            keys = v.keys,
+            module = v.module,
+            cmd = v.cmd,
+        })
+    end
 end
 
 return require("packer").startup(fn)
