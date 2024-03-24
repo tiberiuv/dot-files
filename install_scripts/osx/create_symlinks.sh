36b371cb5b5149006059883503f1854a66634b0d
Tiberiu Voicu <ts.voicu@icloud.com>
2024-03-24 13:21:56 +0000

    Fix link

--- install_scripts/osx/create_symlinks.sh
+++ install_scripts/osx/create_symlinks.sh
@@ -7,7 +7,7 @@ ROOT_DIR="$(git rev-parse --show-toplevel)"
 
 ln -sfF "$ROOT_DIR"/.zshrc "$HOME"/.zshrc
 ln -sfF "$ROOT_DIR"/starship.toml "$HOME"/.config/starship.toml
-ln -sfF "$ROOT_DIR"/.alacritty.toml "$HOME"/.config/alacritty/.alacritty.toml
+ln -sfF "$ROOT_DIR"/alacritty.toml "$HOME"/.config/alacritty/alacritty.toml
 ln -sfF "$ROOT_DIR"/.tmux.conf "$HOME"/.tmux.conf
 ln -sfF "$ROOT_DIR"/lua "$HOME"/.config/nvim/
 ln -sfF "$ROOT_DIR"/init.lua "$HOME"/.config/nvim/init.lua
