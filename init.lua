c45f52f2462b812ea70f3eb04df23017a7209b74
Tiberiu Voicu <ts.voicu@icloud.com>
2024-08-02 17:15:31 +0100

    Re-arrange lazy config

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ local require_reset = require("utils").require_reset
 
 require_reset("options")
 require_reset("keybinds")
-require_reset("plugins")
+-- require_reset("plugins")
 require_reset("autocmd")
 
-
+require("config.lazy")
