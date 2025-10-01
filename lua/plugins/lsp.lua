241832876dde219e36971e5be1cadd7fa3890018
Tiberiu Voicu <ts.voicu@icloud.com>
2025-10-01 23:54:40 +0100

    feat: add cspell linter
    checks grammar and spelling
    only enabled for tex and commits and it's too noisy

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,11 +12,25 @@ return {
                 "-x",
                 "@commitlint/config-conventional"
             }
+            lint.linters.cspell.args = {
+                'lint',
+                '--no-color',
+                '--no-progress',
+                '--no-summary',
+                "--locale",
+                "en-GB",
+                function() return 'stdin://' .. vim.api.nvim_buf_get_name(0) end,
+            }
+            lint.linters.cspell = require("lint.util").wrap(lint.linters.cspell, function(diagnostic)
+                diagnostic.severity = vim.diagnostic.severity.HINT
+                return diagnostic
+            end)
             lint.linters_by_ft = {
                 python = { "flake8", "ruff" },
                 javascript = { "eslint_d" },
                 shell = { "shellcheck" },
                 markdown = { "markdownlint-cli2" },
+                tex = { "cspell" }
             }
             vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
                 callback = function()
@@ -28,6 +42,7 @@ return {
                     local filetype = vim.bo.filetype
                     if filetype == "gitcommit" then
                         require("lint").try_lint("commitlint")
+                        require("lint").try_lint("cspell")
                     end
                 end,
             })
