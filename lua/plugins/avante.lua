d5dd7f53584ef3ada2cc6f85125ce03c8cd792ff
Tiberiu Simion Voicu <tiberiu.voicu@moodys.com>
2026-08-12 13:32:43 +0100

    feat: avante changes + add render-markdown

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,89 +1,108 @@
 return {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  version = false, -- Never set this value to "*"! Never!
-  opts = {
-    system_prompt = function()
-      local hub = require("mcphub").get_hub_instance()
-      return hub and hub:get_active_servers_prompt() or ""
-    end,
-    -- Using function prevents requiring mcphub before it's loaded
-    custom_tools = function()
-      return {
-        require("mcphub.extensions.avante").mcp_tool(),
-      }
-    end,
-    provider = "copilot",
-    providers = {
-      copilot = {
-        model = "claude-sonnet-4"
-      },
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "claude-sonnet-4",      -- your desired model (or use gpt-4o, etc.)
-        extra_request_body = {
-          timeout = 30000,              -- Timeout in milliseconds, increase this for reasoning models
-          temperature = 0.75,
-          max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-          --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+        system_prompt = function()
+            local hub = require("mcphub").get_hub_instance()
+            return hub and hub:get_active_servers_prompt() or ""
+        end,
+        -- Using function prevents requiring mcphub before it's loaded
+        custom_tools = function()
+            return {
+                require("mcphub.extensions.avante").mcp_tool(),
+            }
+        end,
+        provider = "copilot",
+        providers = {
+            openai = {
+                endpoint = "https://api.openai.com/v1",
+                model = "claude-sonnet-4", -- your desired model (or use gpt-4o, etc.)
+                extra_request_body = {
+                    timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+                    temperature = 0.75,
+                    max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+                    --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+                },
+            },
         },
-      },
+        config = function()
+            require("avante").setup()
+        end,
     },
-    config = function()
-      require('avante').setup()
-    end
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = vim.fn.has("win32") ~= 0
-      and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-      or "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-    "stevearc/dressing.nvim",        -- for input provider dressing
-    "folke/snacks.nvim",             -- for input provider snacks
-    "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua",        -- for providers='copilot'
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        or "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    behaviour = {
+        support_paste_from_clipboard = true,
+        auto_suggestions = false,
+        enable_cursor_planning_mode = true,
+        enable_claude_text_editor_tool_mode = true,
+    },
+    mappings = {
+        ask = "ca",
+        edit = "ce",
+        refresh = "cr",
+        diff = {
+            ours = "co",
+            theirs = "ct",
+            none = "c0",
+            both = "cb",
+            next = "]x",
+            prev = "[x",
         },
-      },
+        jump = {
+            next = "]]",
+            prev = "[[",
+        },
     },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-    {
-      "ravitemer/mcphub.nvim",
-      dependencies = {
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
         "nvim-lua/plenary.nvim",
-      },
-      build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
-      config = function()
-        require("mcphub").setup()
-      end
-    }
-  },
+        "MunifTanjim/nui.nvim",
+        --- The below dependencies are optional,
+        "echasnovski/mini.pick", -- for file_selector provider mini.pick
+        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+        "ibhagwan/fzf-lua", -- for file_selector provider fzf
+        "stevearc/dressing.nvim", -- for input provider dressing
+        "folke/snacks.nvim", -- for input provider snacks
+        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        "zbirenbaum/copilot.lua", -- for providers='copilot'
+        {
+            -- support for image pasting
+            "HakonHarnes/img-clip.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- recommended settings
+                default = {
+                    embed_image_as_base64 = false,
+                    prompt_for_file_name = false,
+                    drag_and_drop = {
+                        insert_mode = true,
+                    },
+                    -- required for Windows users
+                    use_absolute_path = true,
+                },
+            },
+        },
+        {
+            -- Make sure to set this up properly if you have lazy=true
+            "MeanderingProgrammer/render-markdown.nvim",
+            opts = {
+                file_types = { "markdown", "Avante" },
+            },
+            ft = { "markdown", "Avante" },
+        },
+        {
+            "ravitemer/mcphub.nvim",
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+            },
+            build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+            config = function()
+                require("mcphub").setup()
+            end,
+        },
+    },
 }
